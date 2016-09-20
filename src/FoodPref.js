48ab94088b4974226c4a5a94ec6906139f2b817a
josephrstraub <josephrstraub@gmail.com>
2016-09-20 16:27:52 -0400

    using children props in App

--- src/FoodPref.js
+++ src/FoodPref.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+const categories = ["American", "Asian", "Indian", "Italian", "Mexican", "South American"];
 
 class FoodPref extends Component {
   render () {
+    let checkBoxes = categories.map(category => (
+      <label><input type="checkbox"/>{category}</label>
+    ))
     return (
-
+      <div>
+        <h2>Food Preferences</h2>
+        <form>
+          {checkBoxes}
+        </form>
+      </div>
     )
   }
 }
