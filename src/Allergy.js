7c0975e8e9716411983a8f332e3f525900a0fc22
Brian H <bheptin@gmail.com>
2016-09-20 10:54:14 -0400

    setting up Components

---
+++ src/Allergy.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Allergy extends Component {
+  render () {
+    return (
+
+    )
+  }
+}
+
+export default Allergy;
---
+++ src/FoodPref.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class FoodPref extends Component {
+  render () {
+    return (
+
+    )
+  }
+}
+
+export default FoodPref;
