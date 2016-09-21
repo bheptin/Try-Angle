3d77750c09bea6d96e012c84cff5c462f9800b83
josephrstraub <josephrstraub@gmail.com>
2016-09-21 13:28:39 -0400

    fixed merge conflict in home--for real

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Choose from './Choose';
-=======
 import Friends from './Friends.js';
->>>>>>> origin/develop
 
 class Home extends Component {
   render () {
