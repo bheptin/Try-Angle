2f43eb264a9a49f148d643c85ad7911aa7c66833
josephrstraub <josephrstraub@gmail.com>
2016-09-21 12:13:04 -0400

    fixed data structure for firebase posts

--- src/FoodPref.js
+++ src/FoodPref.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react';
+import Allergy from './Allergy';
 import base from './config/ReBase';
+import _ from 'lodash';
 
 const categories = ["American", "Asian", "Indian", "Italian", "Mexican", "South American"];
 
 class FoodPref extends Component {
   constructor() {
     super();
-    this.state = {foodPrefs: []};
-    this.handleChange = this.handleChange.bind(this);
-  }
-  componentDidMount() {
-    base.syncState(`users/${this.props.uid}/foodPrefs`, {
-      context: this,
-      state: 'foodPrefs',
-      asArray: true
-    })
-  }
-  handleChange(event) {
-    let selectedFood = event.target.value;
-    if ( !(this.state.foodPrefs.includes(selectedFood)) ) {
-      this.setState({foodPrefs: [...this.state.foodPrefs, selectedFood]});
+    this.state = {
+      allergies: []
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.alterAllergies = this.alterAllergies.bind(this);
+  }
+  alterAllergies(allergies) {
+    this.setState({allergies});
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    let selectedPrefs = _.keysIn(_.pickBy(this.refs, 'checked'));
+    let selectedAllergies = this.state.allergies;
+    this.postToFirebase('foodPrefs', selectedPrefs);
+    this.postToFirebase('allergies', selectedAllergies);
+  }
+  postToFirebase(endpoint, data) {
+    base.post(`users/${this.props.uid}/${endpoint}`, {data});
   }
   render () {
     let checkBoxes = categories.map((category, index) => (
       <label key={index}>
-        <input type="checkbox" onChange={this.handleChange} value={category}/>
+        <input type="checkbox" ref={category}/>
         {category}
       </label>
     ));
     return (
       <div>
         <h2>Food Preferences</h2>
-        {checkBoxes}
+        <form onSubmit={this.handleSubmit}>
+          {checkBoxes}
+          <Allergy handleCheck={this.alterAllergies}/>
+          <button type="submit">Next</button>
+        </form>
       </div>
     )
   }
