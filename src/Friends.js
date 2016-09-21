8f8b20f84e75cd4732d742047ebe25f8cd459fce
Brian H <bheptin@gmail.com>
2016-09-21 13:24:18 -0400

    friends list working

---
+++ src/Friends.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import base from './config/ReBase';
+
+class Friends extends Component {
+  constructor (props) {
+    super(props)
+      this.state = {
+        data: []
+      }
+
+  }
+  componentDidMount(){
+  base.fetch(`users`, {
+    context: this,
+    asArray: true
+  }).then(data => {
+    this.setState({data})
+    console.log(data);
+  }).catch(error => {
+    console.log(error);
+  })
+}
+
+  render () {
+    let fullNames = this.state.data.map((user, index) => <li key={index}>{user.personalInfo.firstName} {user.personalInfo.lastName}</li>);
+    return(
+      <div className="friendsList">
+        <ul style={{listStyleType: "none"}}>{fullNames}
+
+        </ul>
+
+      </div>
+    )
+  }
+
+}
+export default Friends;
