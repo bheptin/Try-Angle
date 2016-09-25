f0f58f0b7da28c912b7c1a6c4e416bc094099842
Brian H <bheptin@gmail.com>
2016-09-25 15:36:21 -0400

    bugs bugs

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -19,7 +19,8 @@ class Choose extends Component {
 
     })
   }
-  render () {      
+  render () {
+    console.log("this");
   let myChoices = this.props.userPrefs.map((mine, index) => <li key={index}>{choice}</li> );
   let theirChoices = this.state.friendId.map(choice => <label><input type="checkbox"/></label>);
     return (
