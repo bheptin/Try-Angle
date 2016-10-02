edf1a063007dac1af2ad75a898bb92e64b1198b9
josephrstraub <josephrstraub@gmail.com>
2016-10-01 21:28:41 -0400

    mobile friendly

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
+import MediaQuery from 'react-responsive';
+import 'react-select/dist/react-select.css';
+import { getRestaurants } from '../config/api';
 import _ from 'lodash';
 import base from '../config/ReBase';
 
 class Choose extends Component {
   constructor() {
     super();
+    this.state = {restaurants: [], value: []};
     this.handleClick = this.handleClick.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  componentWillMount() {
+    this.setState({restaurants: this.props.allRestaurants});
   }
   handleClick() {
-    let selectedRestaurants = _.keysIn(_.pickBy(this.refs, 'checked'));
+    let selectedRestaurants = this.state.value.map(restaurant => restaurant.id);
     base.post(`parties/${this.props.partyId}/selections/${base.auth().currentUser.uid}`, {
       data: selectedRestaurants
     });
@@ -17,21 +27,50 @@ class Choose extends Component {
     });
     this.context.router.push("waiting-room");
   }
+  handleCheck(restaurant, event) {
+    if (event.target.checked) {
+      this.setState({value: [...this.state.value, {value: restaurant.id, label: restaurant.name}]});
+    } else {
+      let value = this.state.value.filter(chosenRestaurant => chosenRestaurant.value !== restaurant.id);
+      this.setState({value});
+    }
+  }
+  updateRestaurants(searchTerm) {
+    getRestaurants(searchTerm).then(restaurants => this.setState({restaurants}));
+  }
+  handleSelect(value) {
+    console.log(value);
+    this.setState({value});
+  }
   render() {
     let { allRestaurants } = this.props;
-    let checkboxes = allRestaurants.map((restaurant, index) => (
+    console.log(this.state.restaurants);
+    let checkboxes = this.state.restaurants.map((restaurant, index) => (
       <label key={index}>
-        <input ref={restaurant.id} type="checkbox" aria-label="..."/>
+        <input ref={restaurant.id} type="checkbox" aria-label="..."
+          checked={this.state.value.filter(i => i.value === restaurant.id).length}
+          onChange={this.handleCheck.bind(this, restaurant)}/>
         {restaurant.name}
         <img src={restaurant.image_url} style={{width: "40px", height: "40px"}} alt="..." className="img-thumbnail"/>
       </label>
     ));
+    let options = this.state.restaurants.map(restaurant => {
+      return {value: restaurant.id, label: restaurant.name}
+    });
     return (
       <div style={{border: "3px solid green"}}>
-        <div className="my-choices">
+        <MediaQuery query='(min-width: 701px)'>
           <h2>What do you want to eat?</h2>
           {checkboxes}
-        </div>
+        </MediaQuery>
+        <MediaQuery query='(max-width: 700px)'>
+          <Select
+            multi={true}
+            value={this.state.value}
+            placeholder="What do you want to eat?"
+            options={options}
+            onChange={this.handleSelect}/>
+        </MediaQuery>
         <button onClick={this.handleClick}>Next</button>
       </div>
     )
