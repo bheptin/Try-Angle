f7a2cc05fe4583626349ced0c8f5f3b6bf34f87a
josephrstraub <josephrstraub@gmail.com>
2016-10-02 13:18:13 -0400

    fixed rerouting bug

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,6 @@ class Login extends Component {
     this.authHandler = this.authHandler.bind(this);
   }
   authHandler (error, userData) {
-    console.log("yooooo");
     if (error) {
       console.log(error)
     } else {
@@ -45,23 +44,21 @@ class Login extends Component {
   render () {
     return (
         <div>
-        <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group" style={{float: "left", marginLeft: "30px"}}>
+              <label>Email address</label>
+              <input type="email" ref="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter email"/>
+              <label>Password</label>
+              <input type="password" ref="password" className="form-control" id="exampleInputPassword" placeholder="Password"/>
+              <button style={{marginTop: "10px"}} type="submit" className="btn btn-primary">Log in</button>
+            </div>
+          </form>
           <div className="signup" style={{float: "right", width: "300px", height: "300px", marginRight: "200px", textAlign: "center"}}>
             <p>Have you ever experienced this?</p>
             <img style={{width: "300px", height: "300px", textAlign: "center"}} src="https://cdn.meme.am/instances/500x/59857655.jpg"/>
             <p> With Try-Angle, take the guesswork of where to eat out! Pair with one or more frineds to make eating out an easy fix!</p>
-            <Link to="signup"><button className="btn btn-secondary">Sign Up!</button></Link>
+            <Link to="signup"><button>Sign Up!</button></Link>
           </div>
-
-          <div className="form-group" style={{float: "left", marginLeft: "30px"}}>
-            <label>Email address</label>
-            <input type="email" ref="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter email"/>
-            <label>Password</label>
-            <input type="password" ref="password" className="form-control" id="exampleInputPassword" placeholder="Password"/>
-            <button style={{marginTop: "10px"}} type="submit" className="btn btn-primary">Log in</button>
-          </div>
-        </form>
-
       </div>
     )
   }
