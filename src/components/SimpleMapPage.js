f713b59e2d713f07821ed5aa62024065c0c5fde1
Brian H <bheptin@gmail.com>
2016-10-09 18:13:10 -0400

    clean up smalls

--- src/components/SimpleMapPage.js
+++ src/components/SimpleMapPage.js
@@ -8,14 +8,15 @@ class SimpleMapPage extends Component {
 
   render() {
 console.log(this.props.lat);
+console.log(this.props.lng);
     return (
       <div className="MapPage">
          <GoogleMap bootstrapURLKeys={{key: 'AIzaSyBL8UkkbIv6jv4eoA3Ci2xFISW2fsdMNRw'}}
          style={{marginTop: "30px", display: "flex", marginLeft: "320px", position: "relative", width: "600px", height: "600px"}}
          defaultZoom={14}
-         defaultCenter={{ lat: 28.5411635 , lng: -81.3812316 }}
+         defaultCenter={{ lat: this.props.lat , lng: this.props.lng }}
          >
-        
+
         </GoogleMap>
       </div>
     );
