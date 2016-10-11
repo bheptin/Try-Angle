235cca52c248e614b4929ec96b9259e646408204
josephrstraub <josephrstraub@gmail.com>
2016-10-11 13:41:03 -0400

    removed temp file

--- src/components/bla.js
+++
@@ -1,5 +0,0 @@
-<div style={this.state.navIsVisible ? {display: "block"} : {display: "none"}}>
-  <Link to="/waiting-room" style={{float: "right", margin: "8px", color: "#DADBEC"}}>Waiting Room</Link>
-  <Link to="/profile" style={{float: "right", margin: "8px", color: "#DADBEC"}}>Profile</Link>
-  <Link to="/login" style={{float: "right", margin: "8px", color: "#DADBEC"}}>Sign Out<span className="sr-only">(current)</span></Link>
-</div>
