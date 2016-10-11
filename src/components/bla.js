1be391d2fb7eb21919a5c776726c0cc45a207bb7
josephrstraub <josephrstraub@gmail.com>
2016-10-11 13:39:53 -0400

    listening for invites for new users

---
+++ src/components/bla.js
@@ -0,0 +1,5 @@
+<div style={this.state.navIsVisible ? {display: "block"} : {display: "none"}}>
+  <Link to="/waiting-room" style={{float: "right", margin: "8px", color: "#DADBEC"}}>Waiting Room</Link>
+  <Link to="/profile" style={{float: "right", margin: "8px", color: "#DADBEC"}}>Profile</Link>
+  <Link to="/login" style={{float: "right", margin: "8px", color: "#DADBEC"}}>Sign Out<span className="sr-only">(current)</span></Link>
+</div>
